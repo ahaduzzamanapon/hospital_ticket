3cacf352dc55b8d8d1b17998ae4a303223b19f25
shihab44 <kshihab9226@gmail.com>
2025-01-21 17:09:37 +0600

    page height done

--- resources/views/site/layout/header.blade.php
+++ resources/views/site/layout/header.blade.php
@@ -3,7 +3,7 @@
     <style>
         .section-title p {
             text-align: center;
-            font-size: 30px;
+            font-size: 20px;
             color: #2C2D3F;
             font-weight: 700;
         }
@@ -20,11 +20,11 @@
 
     <header class="header" >
         <div class="col-lg-12">
-            <div class="bookTicket-logo d-flex justify-content-center">
-                <img src="{{ asset('storage/' . $setting->logo) }}" width="130" alt="">
+            <div class="bookTicket-logo d-flex justify-content-center mt-2">
+                <img src="{{ asset('storage/' . $setting->logo) }}" width="90" alt="">
             </div>
-            <div class="section-title mt-3">
-                <p class="text-center text-xl text-black">E-TICKETING PLATFORM</p>
+            <div class="section-title my-1">
+                <p class="text-center text-xl text-black mt-0">E-TICKETING PLATFORM</p>
             </div>
             
         </div>
